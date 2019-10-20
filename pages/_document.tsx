cfcb9443016994dd8f6242e0660ec44ef5c2ff16
getdaydream <getdaydream@gmail.com>
2019-10-20 16:41:49 +0800

    chore: supoort baseui, styletron

---
+++ pages/_document.tsx
@@ -0,0 +1,45 @@
+import Document, {
+  Html,
+  Main,
+  NextScript,
+  DocumentContext,
+  Head,
+} from 'next/document';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { styletron } from '../styletron';
+import { Sheet } from 'styletron-engine-atomic';
+
+class MyDocument extends Document<{ stylesheets: Sheet[] }> {
+  static async getInitialProps(props: DocumentContext) {
+    const page = await props.renderPage(App => props => (
+      <StyletronProvider value={styletron}>
+        <App {...props} />
+      </StyletronProvider>
+    ));
+    const stylesheets = styletron.getStylesheets() || [];
+    return { ...page, stylesheets };
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          {this.props.stylesheets.map((sheet, i) => (
+            <style
+              className="_styletron_hydrate_"
+              dangerouslySetInnerHTML={{ __html: sheet.css }}
+              media={sheet.attrs.media}
+              data-hydrate={sheet.attrs['data-hydrate']}
+              key={i}
+            />
+          ))}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
